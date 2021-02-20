869822dcc87cfebf7120c43904a59502b8147bca
Vaibhav Jain <kaps12271@gmail.com>
2021-02-20 20:12:50 +0530

    Added logging

--- src/main/java/com/n26/extractor/TransactionExtractor.java
+++ src/main/java/com/n26/extractor/TransactionExtractor.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.n26.entities.Transaction;
 import com.n26.exceptions.InvalidJSONException;
 import com.n26.exceptions.ParsingException;
+import com.n26.validator.TransactionValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -20,6 +23,8 @@ import java.util.Set;
  * Utility class responsible for Extraction of Transaction from given JSON string
  */
 public class TransactionExtractor {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionValidator.class);
+
     private static final String TIMESTAMP_ATTRIB = "timestamp";
     private static final String AMOUNT_ATTRIB = "amount";
 
@@ -47,6 +52,7 @@ public class TransactionExtractor {
             String amountString = jsonNode.get(AMOUNT_ATTRIB).textValue();
             transaction = new Transaction(new BigDecimal(amountString), timestampInMillis / 1000L);
         } catch (IOException | NumberFormatException | DateTimeParseException e) {
+            logger.error(String.format("Issue while parsing JSON: %s", e.getMessage()));
             throw new ParsingException("Issue while paring JSON fields for transaction");
         }
 
--- src/main/java/com/n26/validator/TransactionValidator.java
+++ src/main/java/com/n26/validator/TransactionValidator.java
@@ -3,6 +3,8 @@ package com.n26.validator;
 import com.n26.entities.Transaction;
 import com.n26.exceptions.InvalidTimestampException;
 import com.n26.exceptions.ParsingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Instant;
 
@@ -10,12 +12,16 @@ import java.time.Instant;
  * Validator for Transactions
  */
 public class TransactionValidator {
+    private static final Logger logger = LoggerFactory.getLogger(TransactionValidator.class);
 
     public static void validate(Transaction transaction, long evictionTimeInSeconds) {
         if (transaction.getTimestamp() <= evictionTimeInSeconds)
             throw new InvalidTimestampException();
 
-        if (transaction.getTimestamp() > ((Instant.now().toEpochMilli()) / 1000L))
+        if (transaction.getTimestamp() > ((Instant.now().toEpochMilli()) / 1000L)) {
+            logger.info(String.format("Timestamp is of future: %s", transaction.getTimestamp()));
             throw new ParsingException("Timestamp is of future");
+        }
+
     }
 }
