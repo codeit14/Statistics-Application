e2a3747382cc95925b0d16a2eb7d0fcdb9e39be4
Vaibhav Jain <kaps12271@gmail.com>
2021-02-20 15:54:44 +0530

    Removed unnecessary constant

--- src/main/java/com/n26/constants/TransactionConstants.java
+++ src/main/java/com/n26/constants/TransactionConstants.java
@@ -4,5 +4,4 @@ import java.time.ZoneOffset;
 
 public final class TransactionConstants {
     public static final long EVICTION_TIMESTAMP_LIMIT_IN_SECONDS = 60L;
-    public static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
 }
